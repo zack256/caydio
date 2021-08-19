745dd826f7a6255599b93009e09554486f660523
zack256 <zack256@users.noreply.github.com>
2021-08-19 01:19:24 -0400

    user adds and db config changes

--- app/app_config.py
+++ app/app_config.py
@@ -1,6 +1,12 @@
 import os
+
+def fix_pg_db_url(s):
+    if s.startswith('postgres://"'):
+        s = s.replace("postgres://", "postgresql://", 1)
+    return s
+
 def configure_app(app):
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
+    app.config["SQLALCHEMY_DATABASE_URI"] = fix_pg_db_url(os.environ["DATABASE_URL"])
     app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["SECRET_KEY"] = os.environ["FLASK_SECRET_KEY"]
--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, request, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
+#from sqlalchemy import func as sql_func
 import app.app_config as app_config
 import app.encryption as encryption
 import os
@@ -14,6 +15,7 @@ class User(db.Model):
     username = db.Column(db.String(50), unique = True)
     password = db.Column(db.String(255))
     email = db.Column(db.String(255), unique = True)
+    registered = db.Column(db.DateTime(), default = db.func.now())
 
 def create_user_from_form(username, password_plaintext, email):
     user = User()
@@ -32,6 +34,12 @@ def get_asset_file(file_path):
 def home_page():
     return "welcome to caydio!"
 
+def ci_username_exists(username):
+    # Case-Insensitive check.
+    return User.query.filter(User.username.ilike(username)).first()
+def ci_email_exists(email):
+    return User.query.filter(User.email.ilike(email)).first()
+
 @app.route("/register/", methods = ["GET", "POST"])
 def register_page():
     if request.method == "GET":
@@ -39,5 +47,16 @@ def register_page():
     username = request.form["username"]
     password_plaintext = request.form["password"]
     email = request.form["email"]
+    if ci_email_exists(email):
+        return "Email already exists!"
+    if ci_username_exists(username):
+        return "Username already exists!"
     create_user_from_form(username, password_plaintext, email)
     return "successfully created."
+
+@app.route("/users/<username>/")
+def user_page(username):
+    viewing_user = ci_username_exists(username)
+    if not viewing_user:
+        return "User does not exist!"
+    return "User {} registered at {}.".format(viewing_user.username, viewing_user.registered)
